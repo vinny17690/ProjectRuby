e4ab1f6d7dbbba59b0b9f63f9c1370bc73d08883
vinny <vinay.vinny18@gmail.com>
2015-11-24 20:54:16 -0500

    New Post issue Solved

--- RubyProject/app/controllers/posts_controller.rb
+++ RubyProject/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
